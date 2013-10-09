0142acfe71f4dc471208914a558ddd11c3d8ef0e
cooncesean <cooncesean@gmail.com>
2013-10-09 16:09:12 -0700

    Adding logic to hook up the UI to allow the user to change call_records via the select input.

--- remotestatus/views.py
+++ remotestatus/views.py
@@ -15,8 +15,15 @@ def remote_box_detail(request, remote_box_id):
 @staff_member_required
 def dashboard(request):
     " Show the most recent status update. "
-    call_round = CallRound.objects.latest('id')
-    call_rounds = CallRound.objects.all().order_by('-id')
+    # Get the last 15 call rounds
+    call_rounds = CallRound.objects.all().order_by('-id')[0:15]
+
+    # Optionally select a different call_round than the latest
+    if 'call_round' in request.POST:
+        call_round = get_object_or_404(CallRound, id=request.POST.get('call_round'))
+    else:
+        call_round = CallRound.objects.latest('id')
+
     return render(request, 'remotestatus/dashboard.html', {
         'call_round': call_round,
         'call_rounds': call_rounds,
